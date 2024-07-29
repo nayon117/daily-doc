2297cd78a4dc7fc49aa84ac68587851957ba1d66
nayon117 <hasibul.nayon1@gmail.com>
2024-07-29 19:28:26 +0600

    implement clerk authentication

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const Document = () => {
   return (
@@ -8,6 +9,12 @@ const Document = () => {
         <div className="flex w-fit items-center justify-center gap-2">
           <p className="document-title">title</p>
         </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 
 import { cn } from "@/lib/utils"
 import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -16,6 +18,13 @@ export const metadata:Metadata = {
 
 export default function RootLayout({ children }:{children:React.ReactNode}) {
   return (
+    <ClerkProvider appearance={{
+      baseTheme:dark,
+      variables:{
+        colorPrimary:"#3371ff",
+        fontSize:"16px",
+      }
+    }} >
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
@@ -26,5 +35,6 @@ export default function RootLayout({ children }:{children:React.ReactNode}) {
         {children}
       </body>
     </html>
+    </ClerkProvider>
   )
 }
