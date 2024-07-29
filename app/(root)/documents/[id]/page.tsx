c064271c3eb7fb080fb2c7710c2d9695c1816df7
nayon117 <hasibul.nayon1@gmail.com>
2024-07-29 16:49:51 +0600

    setup files and folder structure

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,9 @@
+
+const Document = () => {
+    return (
+        <div className=''>
+                Welcome to Document
+        </div>
+    )
+}
+export default Document;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+
+const Home = () => {
+    return (
+        <div className='min-h-screen'>
+                Welcome to Home
+        </div>
+    )
+}
+export default Home;
