0069e05113e7a092ac41ecc6b26ea411afe17e10
nayon117 <hasibul.nayon1@gmail.com>
2024-07-31 21:27:21 +0600

    implement notifications

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn";
 import { DeleteModal } from "@/components/DeleteModal";
 import Header from "@/components/Header";
+import Notifications from "@/components/Notifications";
 import { getDocuments } from "@/lib/actions/room.action";
 import { dateConverter } from "@/lib/utils";
 import { SignedIn,UserButton } from "@clerk/nextjs";
@@ -20,7 +21,7 @@ const Home = async() => {
     <main className="home-container">
       <Header className="sticky left-0 top-0">
         <div className="flex items-center gap-2 lg:gap-2">
-          Notifications
+          <Notifications />
           <SignedIn>
             <UserButton />
           </SignedIn>
