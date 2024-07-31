2e1b19aa5f46f618d350bc1f1e03f78ae685e91a
nayon117 <hasibul.nayon1@gmail.com>
2024-07-31 07:20:50 +0600

    fetch document on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,19 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn";
 import Header from "@/components/Header";
+import { getDocuments } from "@/lib/actions/room.action";
+import { dateConverter } from "@/lib/utils";
 import { SignedIn,UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 const Home = async() => {
 
     const clerkUser = await currentUser();
     if(!clerkUser) redirect('/sign-in');
-    const documents = [];
+    
+    const roomDocuments = await getDocuments(clerkUser.emailAddresses[0].emailAddress);
 
   return (
     <main className="home-container">
@@ -23,9 +27,37 @@ const Home = async() => {
       </Header>
 
       
-      {documents.length > 0 ? (
-        <div>
+      {roomDocuments.data.length > 0 ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className="text-28-semibold">All documents</h3>
+            <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+          <ul className="document-ul">
+            {roomDocuments.data.map(({id,metadata,createdAt}:any)=>(
+              <li key={id} className="document-list-item">
+                <Link href={`/documents/${id}`} className="flex flex-1 items-center gap-4">
+                <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                  <Image 
+                  src='/assets/icons/doc.svg'
+                  alt="document"
+                  width={40}
+                  height={40}
+                  />
+                </div>
 
+                <div className="space-y-1">
+                    <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                    <p className="text-sm font-light text-blue-100">created about {dateConverter(createdAt)}</p>
+                </div>
+                </Link>
+                {/* TODO: ADD A DELETE BTN */}
+              </li>
+            ))}
+          </ul>
         </div>
       ):(
         <div className="document-list-empty">
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -44,6 +44,7 @@ const CollaborativeRoom = ({roomId,roomMetadata}:CollaborativeRoomProps) => {
     const handleClickOutside = (e:MouseEvent) => {
       if(containerRef.current && !containerRef.current.contains(e.target as Node)){
         setEditing(false);
+        updateDocument(roomId, documentTitle);
       }
     }
 
@@ -52,7 +53,7 @@ const CollaborativeRoom = ({roomId,roomMetadata}:CollaborativeRoomProps) => {
     return ()=>{
       document.removeEventListener('mousedown',handleClickOutside);
     }
-  },[])
+  },[documentTitle,roomId])
 
   useEffect(()=>{
     if(editing && inputRef.current){
