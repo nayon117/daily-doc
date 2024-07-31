6e45c01df5fe46d5988e29db632e91324e41b5c2
nayon117 <hasibul.nayon1@gmail.com>
2024-07-31 20:33:40 +0600

    update permission system

--- lib/actions/room.action.ts
+++ lib/actions/room.action.ts
@@ -22,7 +22,7 @@ export const createDocument = async({userId,email}:CreateDocumentParams) =>{
         const room = await liveblocks.createRoom(roomId, {
             metadata,
             usersAccesses,
-            defaultAccesses:['room:write']
+            defaultAccesses:[]
           });
           
         revalidatePath('/')
