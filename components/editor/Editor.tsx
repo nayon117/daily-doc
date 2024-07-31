ae5125f2254fd6c5ccdb03185ce8d7483f6049f7
nayon117 <hasibul.nayon1@gmail.com>
2024-07-31 20:44:50 +0600

    implement delete functionality

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -15,6 +15,7 @@ import Loader from "../Loader";
 import FloatingToolbarPlugin from "./plugins/FloatingToolbarPlugin";
 import { useThreads } from "@liveblocks/react/suspense";
 import Comments from "../Comments";
+import { DeleteModal } from "../DeleteModal";
 
 function Placeholder() {
   return <div className="editor-placeholder">Enter some rich text...</div>;
@@ -47,7 +48,7 @@ export function Editor({
       <div className="editor-container size-full">
         <div className="toolbar-wrapper flex min-w-full justify-between">
           <ToolbarPlugin />
-          {/* {currentUserType === "editor" && <DeleteModal roomId={roomId} />} */}
+          {currentUserType === "editor" && <DeleteModal roomId={roomId} />}
         </div>
 
         <div className="editor-wrapper flex flex-col items-center justify-start">
--- lib/actions/room.action.ts
+++ lib/actions/room.action.ts
@@ -4,6 +4,7 @@ import { nanoid } from 'nanoid'
 import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
 import { getAccessType, parseStringify } from '../utils';
+import { redirect } from 'next/navigation';
 
 export const createDocument = async({userId,email}:CreateDocumentParams) =>{
     const roomId = nanoid()
@@ -111,5 +112,15 @@ export const removeCollaborator = async({roomId,email}:{roomId:string,email:stri
 
     } catch (error) {
         console.log(`Error removing collaborator: ${error}`);
+    }
+}
+
+export const deleteDocument = async (roomId:string) =>{
+    try {
+        await liveblocks.deleteRoom(roomId);
+        revalidatePath('/')
+        redirect('/')
+    } catch (error) {
+        console.log(`Error deleting a room: ${error}`);
     }
 }
