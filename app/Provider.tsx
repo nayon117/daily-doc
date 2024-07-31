6f5739133d4574ccd98f2c4addb62f3a7526eedf
nayon117 <hasibul.nayon1@gmail.com>
2024-07-31 08:53:22 +0600

    implement floating comment

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Loader from "@/components/Loader";
-import { getClerkUser } from "@/lib/actions/user.action";
+import { getClerkUsers } from "@/lib/actions/user.action";
 import {
   LiveblocksProvider,
   ClientSideSuspense,
@@ -12,7 +12,7 @@ const Provider = ({ children }: { children: ReactNode }) => {
     <LiveblocksProvider
      authEndpoint="/api/liveblocks-auth"
      resolveUsers={async({userIds})=>{
-        const users = await getClerkUser({userIds});
+        const users = await getClerkUsers({userIds});
         return users ;
      }}
      >
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -11,9 +11,8 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.action";
 
-const CollaborativeRoom = ({roomId,roomMetadata}:CollaborativeRoomProps) => {
+const CollaborativeRoom = ({roomId,roomMetadata,users,currentUserType}:CollaborativeRoomProps) => {
  
-  const currentUserType = 'editor'; // editor | viewer
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
   const [editing, setEditing] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -111,7 +110,7 @@ const CollaborativeRoom = ({roomId,roomMetadata}:CollaborativeRoomProps) => {
             </SignedIn>
             </div>
           </Header>
-          <Editor />
+          <Editor roomId={roomId} currentUserType={currentUserType} />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
